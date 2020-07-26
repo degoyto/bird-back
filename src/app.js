8ec57b18136607d003e33d042f0a5630d2591f38
degoyto <48868413+degoyto@users.noreply.github.com>
2020-07-26 17:23:54 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -14,10 +14,14 @@ const morgan = require("morgan")
 const app = express();
  
 app.use (morgan('combined'))
-app.use(cors({origin: '*'}))
+app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
-
+app.use(function(req, res, next) {
+res.header("Access-Control-Allow-Origin", "*");
+res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+next();
+});
 
 
 
