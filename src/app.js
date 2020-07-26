42fe2d2a159f26f796335fa8b3915ec4e50cc077
degoyto <48868413+degoyto@users.noreply.github.com>
2020-07-26 16:46:33 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ var corsOptions = {
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 app.use (morgan('combined'))
-app.use(cors())
+app.use(cors(corsOptions))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
