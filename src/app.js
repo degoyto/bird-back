66e5403a65f8456630de7bec5a0d535ff7c03549
degoyto <48868413+degoyto@users.noreply.github.com>
2020-07-26 17:11:19 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://192.168.0.9:4200');
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
