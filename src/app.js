0acc76f3b9a1ba9195c877f64567373890f77328
degoyto <48868413+degoyto@users.noreply.github.com>
2020-07-26 17:44:20 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(function(req, res, next) {
-        res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+        res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, X-XSRF-TOKEN, Content-Type, Accept");
         next();
     });
