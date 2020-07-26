32a5a4f25f66d8ba22adc11d8576bca3878ad4ce
degoyto <48868413+degoyto@users.noreply.github.com>
2020-07-26 16:04:05 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -16,7 +16,11 @@ app.use (morgan('combined'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(cors())
-
+app.use(function(req, res, next) {
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+ });
 
 
 //Rotas
